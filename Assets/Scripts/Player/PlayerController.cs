b2fbe4ce0d014a9c4e3301e6c08e3f7e46041c7c
Pendragon64 <78666180+Pendragon64@users.noreply.github.com>
2021-08-29 22:52:09 -0400

    initial movement and basic dash

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //inputs
+    public Vector2 movement;
+    public Vector2 aim;
+    public bool fire;
+
+    //im sorta copying this from last year's project im 90% sure some of it is not necessary
+    Rigidbody2D rb2d;
+    public float movePower = 5f;
+    public float dashCD = 1f;
+    public float lastDash = 0f;
+    public float dashPower = 10f;
+    public GameObject crosshairs;
+    public float crosshairDistance = 4;
+    public Sprite neutral;
+    public Sprite dash;
+    public Transform pos;
+    public SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        aim = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        fire = Input.GetMouseButton(0);
+    }
+
+    void FixedUpdate() {
+        
+        //NOTE FOR FUTURE DEVELOPING:
+        //to make a "dash" use rb2d.AddForce(new Vector2(speed,speed), ForceMode2D.Impulse)
+
+        if(fire && lastDash >= dashCD){
+            rb2d.velocity = new Vector2(0,0);
+            rb2d.AddForce(aim.normalized * crosshairDistance * dashPower, ForceMode2D.Impulse);
+            lastDash = 0f;
+        }
+        lastDash += Time.deltaTime;
+
+        rb2d.AddForce(movement * movePower);
+
+		Debug.DrawRay(transform.position, aim.normalized * crosshairDistance, Color.red);
+
+		// position crosshairs
+		//if(aim.magnitude < crosshairDistance) { 
+			//crosshairs.transform.position = (Vector2) transform.position + aim;
+		//} else {
+		crosshairs.transform.position = (Vector2) transform.position + (aim.normalized * crosshairDistance);
+		//}
+
+		
+	}
+
+}
