92b14c83b55d4f10427b6fc8727a597398ad65b3
Pendragon64 <78666180+Pendragon64@users.noreply.github.com>
2021-08-30 16:07:25 -0400

    Camera and demo goals

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //gameObjects
+    public GameObject ball;
+    public Vector3 ballStart;
+    public GameObject player;
+    public Vector3 playerStart;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballStart = ball.transform.position;
+        playerStart = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ResetGame(){
+        //temp for demo, resets positions
+        ball.GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+        ball.transform.position = ballStart;
+        player.GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+        player.transform.position = playerStart;
+    }
+}
---
+++ Assets/Scripts/Systems/Ball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public GameManager gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.gameObject.name == "Goal"){
+            gm.ResetGame();
+            //temp for demo, will need to move to next scene, see gamemanager
+        }
+    }
+}
