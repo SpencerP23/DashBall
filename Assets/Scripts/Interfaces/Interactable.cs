a51789738f7d7485b5ed239a1d4e5534251dd99d
Pendragon64 <78666180+Pendragon64@users.noreply.github.com>
2021-08-30 17:04:57 -0400

    demo map

---
+++ Assets/Scripts/Interfaces/Interactable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    //TODO: "layer" this so all things that can interact with stuff (balls, players, obstacles?) can interact with them
+    //this stuff being boosters, bouncy walls (like the things in pinball), slow zones, etc 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
